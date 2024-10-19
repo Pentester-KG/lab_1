1440dc8e695c83deea448e9aabf5e148f6b3291f
Pentester-KG <sotbeka.2048@gmail.com>
2024-10-19 14:43:56 +0600

    added function for statistic words

--- main.py
+++ main.py
@@ -25,7 +25,26 @@ def write_word_count_report(words):
             f.write(f"{word}: {count}\n")
 
 
+def write_statistics_report(words):
+    word_lengths = Counter(len(word) for word in words)
+    unique_words = len(set(words))
+    punctuation_count = len(re.findall(r'[^\w\s]', ' '.join(words)))
 
+    report_file = os.path.join('result', 'text1_stat.txt')
+    with open(report_file, 'w', encoding='utf-8') as f:
+        f.write(f"Уникальные слова: {unique_words}\n")
+        f.write(f"Количество знаков препинания: {punctuation_count}\n")
+        for length, count in word_lengths.items():
+            f.write(f"Слова с {length} буквами: {count}\n")
+
+
+def main():
+    if not os.path.exists('result'):
+        os.makedirs('result')
+    text = read_file()
+    words = clean_and_split_text(text)
+    write_word_count_report(words)
+    write_statistics_report(words)
 
 
 
